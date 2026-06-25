c859c816f62332216af9cd6107e7203cf124ce31
hbokok <260413992@qq.com>
2026-06-25 17:17:11 +0800

    fix

--- OmniAdsSdk.podspec
+++ OmniAdsSdk.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'OmniAdsSdk'
-  s.version          = '3.8.6'
+  s.version          = '3.8.7'
   s.summary          = 'OmniAds iOS sdk'
 
 # This description is used to generate tags and improve search results.
--- OmniAdsSdk/Classes/sdk/OmniAdSdk.h
+++ OmniAdsSdk/Classes/sdk/OmniAdSdk.h
@@ -5,9 +5,14 @@
 //
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface OmniAdSdk : NSObject
 +(instancetype) shareInstance;
 -(void)initWithAppId:(NSString *)appId;
+-(void)initWithAppId:(NSString *)appId completion:(void(^)(BOOL success, NSError * _Nullable error))completion;
 -(void)setDebugMode:(BOOL)enable;
 -(void) startAdPrev:(NSString *)rewardId interstitialId:(NSString *)interstitialId fullScreenId:(NSString *)fullScreenId userId:(NSString *)userId;
 @end
+
+NS_ASSUME_NONNULL_END
